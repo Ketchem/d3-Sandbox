7fee37ec1884989a32597bc4bfcc74a703a2dc7d
Brian K <35375130+BrianK@users.noreply.github.com>
2019-03-19 15:17:59 -0400

    updated file path for data

--- assets/js/mapping.js
+++ assets/js/mapping.js
@@ -1,13 +1,13 @@
 //begin script when window loads
 window.onload = setMap();
 
-//set up choropleth map
+//Example 1.3 line 4...set up choropleth map
 function setMap(){
-    //use queue to parallelize asynchronous data loading
+    //use d3.queue to parallelize asynchronous data loading
     d3.queue()
-        .defer(d3.csv, "assets/data/unitsData.csv") //load attributes from csv
-        .defer(d3.json, "assets/data/EuropeCountries.topojson") //load background spatial data
-        .defer(d3.json, "assets/data/FranceProvinces.topojson") //load choropleth spatial data
+        .defer(d3.csv, "../assets/data/unitsData.csv") //load attributes from csv
+        .defer(d3.json, "../assets/data/EuropeCountries.topojson") //load background spatial data
+        .defer(d3.json, "../assets/data/FranceProvinces.topojson") //load choropleth spatial data
         .await(callback);
 
     function callback(error, csvData, europe, france){

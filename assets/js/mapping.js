59842699fbc018b3ef7c2d80a1d1d507344c4342
Brian K <35375130+BrianK@users.noreply.github.com>
2019-03-19 15:46:52 -0400

    Corrected data name error

--- assets/js/mapping.js
+++ assets/js/mapping.js
@@ -7,7 +7,7 @@ function setMap(){
     d3.queue()
         .defer(d3.csv, "assets/data/unitsData.csv") //load attributes from csv
         .defer(d3.json, "assets/data/EuropeCountries.topojson") //load background spatial data
-        .defer(d3.json, "assets/data/FranceProvinces.topojson") //load choropleth spatial data
+        .defer(d3.json, "assets/data/FranceRegions.topojson") //load choropleth spatial data
         .await(callback);
 
     function callback(error, csvData, europe, france){

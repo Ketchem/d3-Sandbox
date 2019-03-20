6add3e57170107d6fd12231e7067de6690a588fd
Brian K <>
2019-03-20 11:17:39 -0400

    Fixed the mapping.js to convert objects to geoJson

--- assets/js/mapping.js
+++ assets/js/mapping.js
@@ -1,74 +1,22 @@
 //begin script when window loads
 window.onload = setMap();
 
-// //Example 1.3 line 4...set up choropleth map
-// function setMap(){
-//     //use d3.queue to parallelize asynchronous data loading
-//     d3.queue()
-//         .defer(d3.csv, "assets/data/unitsData.csv") //load attributes from csv
-//         .defer(d3.json, "assets/data/EuropeCountries.topojson") //load background spatial data
-//         .defer(d3.json, "assets/data/FranceRegions.topojson") //load choropleth spatial data
-//         .await(callback);
-//
-//     function callback(error, csvData, europe, france){
-//         console.log(error);
-//         console.log(csvData);
-//         console.log(JSON.stringify(europe));
-//         console.log(JSON.stringify(france));
-//     };
-//
-// };
 
 function setMap(){
-    var csvData = d3.csv("assets/data/unitsData.csv");
-    // var europe = d3.json("assets/data/EuropeCountries.topojson");
-    // var france = d3.json("assets/data/FranceRegions.topojson");
+    var dataSets = [];
+    dataSets.push(d3.csv("assets/data/unitsData.csv"));
+    dataSets.push(d3.json("assets/data/EuropeCountries.topojson"));
+    dataSets.push(d3.json("assets/data/FranceRegions.topojson"));
 
-    var csvValue;
+    Promise.all(dataSets).then(function(values) {
+        console.log(values[1]);
+        console.log(values[2]);
 
-    csvData.then(function(result) {
-        console.log(result);
-        csvValue = result;
+        var europeCountries = topojson.feature(values[1], values[1].objects.EuropeCountries);
+        var franceRegions = topojson.feature(values[2], values[2].objects.FranceRegions);
+
+        console.log(europeCountries);
+        console.log(franceRegions);
     });
-
-    // console.log(csvData);
-    console.log(csvValue);
-    // console.log(europe);
-    // console.log(france);
-
-
-    // var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
-    //     franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
-    //
-    // //examine the results
-    // console.log(europeCountries);
-    // console.log(franceRegions);
 };
 
-
-// var files = [ "assets/data/EuropeCountries.topojson", "assets/data/FranceRegions.topojson"];
-// var promises = [];
-//
-// promises.push(d3.csv("assets/data/unitsData.csv"));
-//
-// files.forEach(function(url) {
-//     promises.push(d3.json(url))
-// });
-//
-// Promise.all(promises).then(function(values) {
-//     console.log(values)
-// });
-
-// var csvData = d3.csv("assets/data/unitsData.csv");
-// var europe = d3.json("assets/data/EuropeCountries.topojson");
-// var france = d3.json("assets/data/FranceRegions.topojson");
-//
-// csvData.then(function(values) {
-//     console.log(values)
-// });
-// europe.then(function(values){
-//     console.log(values)
-// });
-// france.then(function(values){
-//     console.log(values)
-// });
